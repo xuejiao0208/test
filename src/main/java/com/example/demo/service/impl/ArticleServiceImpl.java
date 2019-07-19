7b8d6bcc666f0cd5a7060a00c4679c748aa4a319
xuejiao0208 <1107952387@qq.com>
2019-07-19 08:00:41 +0800

    first submit

---
+++ src/main/java/com/example/demo/service/impl/ArticleServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.ArticleService;
+
+public class ArticleServiceImpl  implements ArticleService {
+}
---
+++ src/main/java/com/example/demo/service/impl/NavServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.NavService;
+
+public class NavServiceImpl implements NavService {
+}
